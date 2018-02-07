d614d990066fa185d24a33711c7057e9cf30236a
aazarodov <aazarodov@gmail.com>
2018-02-07 09:26:57 +0300

    7

---
+++ src/main/java/servlets/AllRequestsServlet.java
@@ -0,0 +1,8 @@
+package servlets;
+
+/**
+ * Created by zarodov on 07.02.2018.
+ */
+public class AllRequestsServlet {
+
+}
---
+++ src/main/java/templater/PageGenerator.java
@@ -0,0 +1,7 @@
+package templater;
+
+/**
+ * Created by zarodov on 07.02.2018.
+ */
+public class PageGenerator {
+}
